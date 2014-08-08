27d4fe494d43eecff4a4f7c6c3913b14c4bec4e2
thoroc <tam@W530>
2014-08-08 09:04:19 +0100

    fixed mergesort duh

--- sort.go
+++ sort.go
@@ -7,13 +7,8 @@ import (
 func main() {
 	input := []int{1, 3, 7, 2, 9, 8, 4, 6, 5, 0}
 	log.Print("input array: ", input)
-	//log.Print(Shift(input))
-	//for key, value := range Shift(input) {
-	//	log.Print(key, value)
-	//}
-	//insertion := InsertionSort(input)
-	//log.Print(insertion)
-	//MergeSort(input)
+	insertion := InsertionSort(input)
+	log.Print("insertion array: ", insertion)
 	merge := MergeSort(input)
 
 	log.Print("merged array: ", merge)
@@ -39,53 +34,26 @@ func MergeSort(slice []int) []int {
 	// split the slice in 2
 	left := slice[0 : len(slice)/2]
 	right := slice[len(slice)/2:]
-	//log.Print("left + right ", left, right)
 	// repeat
 	left = MergeSort(left)
 	right = MergeSort(right)
 	// merge the slice
-	//log.Print("left + right ", left, right)
 	output := Merge(left, right)
 	return output
 }
 
 func Merge(left []int, right []int) []int {
-
-	length := 0
-	if len(left) > len(right) {
-		length = len(left)
-	} else {
-		length = len(right)
-	}
-	//log.Print("max length: ", length)
-	//result := make([]int, length)
-	//log.Print(result)
+	result := make([]int, 0)
 	for len(left) > 0 && len(right) > 0 {
-
-		// get the head of each array
-		// compare them
-		// taken the smaler item
-		// add it to the result array
-		// shift the array of origin by one
-		//log.Print(i)
 		if left[0] <= right[0] {
-			result := left[0]
-			//result = append(result, left[0])
+			result = append(result, left[0])
 			left = left[1:]
 		} else {
-			result := right[0]
-			//result = append(result, right[0])
+			result = append(result, right[0])
 			right = right[1:]
 		}
-		//log.Print(result)
 	}
 	result = append(result, left...)
 	result = append(result, right...)
-	//log.Print(result)
 	return result
-}
-
-func Shift(slice []int) []int {
-	slice = slice[1:]
-	return slice
 }
