546ce1aa9abfe8c7e1e2dc44893d0faa11458409
thoroc <thomas.a.roche@gmail.com>
2014-09-21 12:58:06 +0100

    mutable and quickfix

--- slice.go
+++ slice.go
@@ -8,5 +8,5 @@ func main() {
 	fmt.Printf("address of first element: %p\n", &slice[0])
 	fmt.Printf("address of slice itself:  %p\n", &slice)
     fmt.Printf("the len is %d and cap is %d \n", len(slice), cap(slice))
-    fmt.Printf("address of slice %p add of Arr %p\n", &slice, &intarr)
+    fmt.Printf("address of slice %p address of Arr %p\n", &slice, &intarr)
 }
