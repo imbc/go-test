c6a5b02031443d4406b85aacad7cf91c6a007d94
thoroc <thomas.a.roche@gmail.com>
2014-09-21 11:06:26 +0100

    bugfix on previous commit

--- ptr_vs_pt_vs_struct.go
+++ ptr_vs_pt_vs_struct.go
@@ -5,19 +5,19 @@ import "fmt"
 // see http://stackoverflow.com/questions/24894479/golangdifferent-about-call-a-method-with-pointer-type-using-point-type-or-str?rq=1
 
 type User struct {
-    Name string
+	Name string
 }
 
-ffunc (u *User) Greeting() string {
-    return fmt.Sprintf("Greetings %s [%p]!", u.Name, u)
+func (u *User) Greeting() string {
+	return fmt.Sprintf("Greetings %s [%p]!", u.Name, u)
 }
 
 func main() {
-    p := &User{"cppgohan by pointer"}
-    u := User{"cppgohan by value"}
-    pu := &u
-
-    fmt.Printf("[%p] %v %v\n", pu, pu.Greeting(), pu)
-    fmt.Printf("[%p] %v %v\n", &u, u.Greeting(), u)
-    fmt.Printf("[%p] %v %v\n", p, p.Greeting(), p)
+	p := &User{"cppgohan by pointer"}
+	u := User{"cppgohan by value"}
+	pu := &u
+	fmt.Printf("[%p] %v %v\n", pu, pu.Greeting(), pu)
+	fmt.Printf("[%p] %v %v\n", &u, u.Greeting(), u)
+	fmt.Printf("[%p] %v %v\n", p, p.Greeting(), p)
 }
+
